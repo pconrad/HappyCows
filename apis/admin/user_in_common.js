270bb5dd79cf64a1e1dbed8816460a0c674cbd56
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-03-31 19:36:31 -0700

    pc - try removing limit on user query

--- apis/admin/user_in_common.js
+++ apis/admin/user_in_common.js
@@ -22,11 +22,11 @@ function get_users_in_common(req, commonId){
 		'SELECT u.id, u.firstName, u.lastName, u.email, u.type, ' +
 		`(${get_wealth} u.id AND CommonId = ?) AS wealth, ` +
 		`(${get_cows} u.id AND CommonId = ?) AS cows ` +
-		'FROM UserCommons AS uc JOIN Users AS u ON u.id = uc.UserId  WHERE uc.CommonId = ? ' +
-		'LIMIT ?, ?',
+		'FROM UserCommons AS uc JOIN Users AS u ON u.id = uc.UserId  WHERE uc.CommonId = ? ' /* +
+		'LIMIT ?, ?' */,
 		{
 			replacements: [
-				commonId,commonId,commonId, ...paging_raw(req)
+				commonId,commonId,commonId /*, ...paging_raw(req) */
 			],
 			type: QueryTypes.SELECT
 		}).then((dbRes)=>{
